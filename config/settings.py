ec14d0615d0dec3546c8badc7de0d687d9d6e023
jiangfeng <jiangfeng@zentech-inc.com>
2018-08-28 16:05:56 +0800

    chushihua tornado

---
+++ config/settings.py
@@ -0,0 +1,10 @@
+#date:2018/8/28
+
+
+#tornado配置
+settings = {
+    'template_path': 'template',
+    'static_path': 'static',
+    'static_url_prefix': '/static/',
+    'cookie_secret' : "61oETzKXQAGaYdkL5gEmGeJJFuYh7EQnp2XdTP1o/Vo=",
+}
